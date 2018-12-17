e73b6b1b866bff05c023af9172124662e1f039d8
Alexandru Laszlo <laszloalexandru2016@gmail.com>
2018-12-18 00:59:43 +0200

    Wrote the code better

--- ProiectMatrice.ino
+++ ProiectMatrice.ino
@@ -1,10 +1,12 @@
 #include "LedControl.h" //  need the library
+#include <LiquidCrystal.h>
+#include <EEPROM.h>
+
 #define JOY_X A0
 #define JOY_Y A1
 #define JOY_BTN 2
 #define MENU_BTN 13
-#include <LiquidCrystal.h>
-#define V0_PIN 9 // PWN in loc de POTENTIOMETRU
+#define V0_PIN 9 
 
 LiquidCrystal lcd(8, 3, 4, 5, 6, 7);
 
@@ -18,7 +20,7 @@ int oldWantToTurn = 0;
 long lastMove = 0;
 const long moveDelay = 100;
 const long dropDelay = 500;
-
+const long menuMoveDelay = 150;
 int oldButton = 0;
 int prevX = 3, prevY = 3, posX = 3, posY = 3;
 int buttonState = 0;
@@ -27,9 +29,10 @@ bool movedX = false, movedY = false;
 int rotation = 0;
 long lastDrop;
 int score = 0;
-int menuItemIndex = 0;
-
+int menuItem = 0;
+int address = 0;
 LedControl lc = LedControl(12, 11, 10, 1); //DIN, CLK, LOAD, No. DRIVER
+
 
 // pin 12 is connected to the MAX7219 pin 1
 // pin 11 is connected to the CLK pin 13
@@ -213,353 +216,387 @@ const bool pieceL2[16][4] = {
   {0, 0, 0, 0}
   
 };
+void EEPROMWriteInt(int value) {
+  byte two = (value & 0xFF);
+  byte one = ((value >> 8) & 0xFF);
 
-void menu(){
+  EEPROM.update(address, two);
+  EEPROM.update(address + 1, one);
+}
 
-    
-  int valY = analogRead(JOY_Y);
+int EEPROMReadInt() {
+  long two = EEPROM.read(address);
+  long one = EEPROM.read(address + 1);
+
+  return ((two << 0) & 0xFFFFFF) + ((one << 8) & 0xFFFFFFFF);
+}
+
+void highScoreReplace() {
+  int highScore = EEPROMReadInt();
+  if (highScore < score)
+    EEPROMWriteInt(score);
+}
+
+void resetGame() {
+  for (int i = 0; i < 8; ++i)
+    for (int j = 0; j < 8; ++j)
+      matrix[i][j] = 0;
+  chooseNewPiece();
+  return;
+}
+
+void menu() {
   int select = 0;
-  if(valX < 400)
-    menuItemIndex +=1;
-  if(valX > 600)
-    menuItemIndex -= 1;
-  if(menuItemIndex > 2)
-    menuItemIndex = 0;
-  switch(menuItemIndex){
+  long int lastMenuMove = lastMove;
+  while (1) {
+
+    if (millis() - lastMenuMove > menuMoveDelay) {
+      lastMenuMove = millis();
+      int valY = analogRead(JOY_Y);
+      if (valY > 600)
+        menuItem++;
+      if (valY < 400)
+        menuItem--;
+      if (menuItem > 2)
+        menuItem = 0;
+      if (menuItem < 0)
+        menuItem = 2;
+      Serial.println(menuItem);
+      switch (menuItem) {
       case 0:
         lcd.clear();
-        lcd.setCursor(2,1);
-        lcd.print("New Game");
+        lcd.print("Reset Game");
         select = digitalRead(JOY_BTN);
-        if(select != 0)
-          //setup();
+        if (select == 1) {
+          resetGame();
+          return;
+        }
         break;
       case 1:
         lcd.clear();
-        lcd.setCursor(2,1);
         lcd.print("High score");
         select = digitalRead(JOY_BTN);
-        if(select != 0)
-          //showHighScore();
+        if (select == 1) {
+          lcd.clear();
+          lcd.print(EEPROMReadInt());
+        }
         break;
       case 2:
         lcd.clear();
-        lcd.setCursor(2,1);
-        lcd.print("Resume game");
+        lcd.print("Return");
         select = digitalRead(JOY_BTN);
-        if(select != 0)
-          return ;
+        if (select == 1)
+          return;
         break;
+      default:
+        break;
+      }
     }
+
   }
 
+}
 
-void chooseNewPiece(){
-  
+//We choose a new piece by randomising a number from 0 to 6 to get the index of the next piece that we will get
+
+void chooseNewPiece() {
+ 
   int i, j, k;
-  j = random()%7;
+  j = random() % 7;
   switch (j) {
-      case 0: 
-        for(i = 0;i<16;i++)
-          for(k = 0;k<4;k++)
-            currentPiece[i][k] = pieceI[i][k];
-        break;
-      case 1: 
-        for(i = 0;i<16;i++)
-          for(k = 0;k<4;k++)
-            currentPiece[i][k] = pieceT[i][k];
-        break;
-      case 2: 
-        for(i = 0;i<16;i++)
-          for(k = 0;k<4;k++)
-            currentPiece[i][k] = pieceZ1[i][k];
-        break;
-      case 3: 
-        for(i = 0;i<16;i++)
-          for(k = 0;k<4;k++)
-            currentPiece[i][k] = pieceZ2[i][k];
-        break;
-      case 4: 
-        for(i = 0;i<16;i++)
-          for(k = 0;k<4;k++)
-            currentPiece[i][k] = pieceSquare[i][k];
-        break;
-      case 5: 
-        for(i = 0;i<16;i++)
-          for(k = 0;k<4;k++)
-            currentPiece[i][k] = pieceL1[i][k];
-        break;
-      case 6: 
-        for(i = 0;i<16;i++)
-          for(k = 0;k<4;k++)
-            currentPiece[i][k] = pieceL2[i][k];
-        break;
-    }  
+  case 0:
+    for (i = 0; i < 16; i++)
+      for (k = 0; k < 4; k++)
+        currentPiece[i][k] = pieceI[i][k];
+    break;
+  case 1:
+    for (i = 0; i < 16; i++)
+      for (k = 0; k < 4; k++)
+        currentPiece[i][k] = pieceT[i][k];
+    break;
+  case 2:
+    for (i = 0; i < 16; i++)
+      for (k = 0; k < 4; k++)
+        currentPiece[i][k] = pieceZ1[i][k];
+    break;
+  case 3:
+    for (i = 0; i < 16; i++)
+      for (k = 0; k < 4; k++)
+        currentPiece[i][k] = pieceZ2[i][k];
+    break;
+  case 4:
+    for (i = 0; i < 16; i++)
+      for (k = 0; k < 4; k++)
+        currentPiece[i][k] = pieceSquare[i][k];
+    break;
+  case 5:
+    for (i = 0; i < 16; i++)
+      for (k = 0; k < 4; k++)
+        currentPiece[i][k] = pieceL1[i][k];
+    break;
+  case 6:
+    for (i = 0; i < 16; i++)
+      for (k = 0; k < 4; k++)
+        currentPiece[i][k] = pieceL2[i][k];
+    break;
+  }
 
   currentPieceX = 3;
   currentPieceY = -4;
   currentPieceRotation = 0;
-  
+
 }
 
-void deleteRow(int y){
-  while(y > 0){
-    for(int x = 0; x < 8; ++x){
-      matrix[y][x] = matrix[y-1][x];
+/* We scan the matrix for rows that are full in the "removeRows" function then we call "deleteRows" sending 
+   it the row index then we copy the rows above one row down and we make the top row empty*/
+
+void deleteRow(int y) {
+  while (y > 0) {
+    for (int x = 0; x < 8; ++x) {
+      matrix[y][x] = matrix[y - 1][x];
     }
     --y;
   }
-  for(int x = 0; x < 8; ++x){
+  for (int x = 0; x < 8; ++x) {
     matrix[0][x] = 0;
   }
   score += 50;
   lcd.clear();
-  lcd.setCursor(2,7);
+  lcd.setCursor(2, 7);
   lcd.print(score);
 }
 
-void removeRows(){
-  int counter; 
-  for(int y = 0; y < 8; ++y){
+void removeRows() {
+  int counter;
+  for (int y = 0; y < 8; ++y) {
     counter = 0;
-    for(int x = 0; x < 8; ++x){
-      if( matrix[y][x] == true ) 
+    for (int x = 0; x < 8; ++x) {
+      if (matrix[y][x] == true)
         counter++;
     }
-    if(counter == 8)
+    if (counter == 8)
       deleteRow(y);
   }
 }
 
-int pieceOffEdge(int pieceX, int pieceY, int pieceRotation){
-    int k = 0;
-    bool newPiece[4][4];  
-    for(int i = pieceRotation * 4;i < pieceRotation * 4 + 4; ++i){
-        for(int j = 0; j < 4; ++j)
-          newPiece[k][j] = currentPiece [i][j];
-        k++;
-      } 
-    for(int i = 0; i < 4; ++i){
-       for(int x = 0; x < 4; ++x) {
-          int newY = pieceY + i;
-          int newX = pieceX + x;
-          if(newY < 0)continue;
-          if(newPiece[i][x] > 0){
-              if(newX >= 8) return 1;
-              if(newX < 0) return 1;
-          }
-       }
-    }
-    return 0;
-}
-
-int pieceHitsBottom(int pieceX, int pieceY, int pieceRotation){
-    int k = 0;
-    bool newPiece[4][4];  
-    for(int i = pieceRotation * 4;i < pieceRotation * 4 + 4; ++i){
-        for(int j = 0; j < 4; ++j)
-          newPiece[k][j] = currentPiece [i][j];
-        k++;
-      } 
-    for(int y = 0; y < 4; ++y) {    
-      int ny = pieceY + y;
-      if(ny<0) continue;
-      for(int x = 0; x < 4; ++x) {
-        int nx = pieceX + x;
-        if(newPiece[y][x] > 0) {
-          if(ny >= 8 ) return 1;  // yes: goes off bottom of grid      
-          if(matrix[ny][nx] == 1 ) return 1;  // yes: grid already full in this space
-        }
+int pieceOffEdge(int pieceX, int pieceY, int pieceRotation) {
+  int k = 0;
+  bool newPiece[4][4];
+  for (int i = pieceRotation * 4; i < pieceRotation * 4 + 4; ++i) {
+    for (int j = 0; j < 4; ++j)
+      newPiece[k][j] = currentPiece[i][j];
+    k++;
+  }
+  for (int i = 0; i < 4; ++i) {
+    for (int x = 0; x < 4; ++x) {
+      int newY = pieceY + i;
+      int newX = pieceX + x;
+      if (newY < 0) continue;
+      if (newPiece[i][x] > 0) {
+        if (newX >= 8) return 1;
+        if (newX < 0) return 1;
       }
-    }
-  return 0;  // doesn't hit
-}
-
-int pieceCanFit(int pieceX, int pieceY, int pieceRotation){
-    if( pieceOffEdge(pieceX, pieceY, pieceRotation) )
-        return 0;
-    if( pieceHitsBottom(pieceX, pieceY, pieceRotation) )
-        return 0;
-    return 1;
-}
-
-
-void erasePiece() {
-    int i, j, k = 0;
-    bool  newPiece[4][4] = {0};
-    for(i = currentPieceRotation * 4; i < currentPieceRotation * 4 + 4; ++i){
-      for(j = 0; j < 4; ++j)
-          newPiece[k][j] = currentPiece[i][j];
-      k++;
-    }
-    for(i = 0; i < 4; ++i)
-      for(j = 0; j < 4; ++j){
-          int nx = currentPieceX + j;
-          int ny = currentPieceY + i;
-          if(ny < 0 || ny > 7)continue;
-          if(nx < 0 || nx > 7)continue;
-          if(newPiece[i][j] == true)
-            matrix[ny][nx] = false; 
-      }
-}
-
-void placePiece() {
-    
-    int i, j, k = 0;
-    bool  newPiece[4][4] = {0};
-    for(i = currentPieceRotation * 4; i < currentPieceRotation * 4 + 4; ++i){
-      for(j = 0; j < 4; ++j)
-          newPiece[k][j] = currentPiece[i][j];
-      k++;
-    }
-    for(i = 0; i < 4; ++i)
-      for(j = 0; j < 4; ++j){
-          int nx = currentPieceX + j;
-          int ny = currentPieceY + i;
-          if(ny < 0 || ny > 7)continue;
-          if(nx < 0 || nx > 7)continue;
-          if(newPiece[i][j] == true)
-            matrix[ny][nx] = true; 
-      }
-}
-
-void movePiece(){
-    int valX = analogRead(JOY_X);
-
-    int newPieceX = 0;
-
-    if(valX < 400)
-       newPieceX = -1;
-    if(valX > 600)
-       newPieceX = 1;
-
-    if(newPieceX != oldPieceX && pieceCanFit(currentPieceX + newPieceX, currentPieceY, currentPieceRotation) == 1){
-        currentPieceX += newPieceX;
-    }
-    oldPieceX = newPieceX;
-    }
-      
-
-void rotatePiece(){
-  int wantToTurn = 0;
-  int newButton = digitalRead(JOY_BTN);
-
-  if(newButton > 0 && oldButton != newButton){
-      wantToTurn = 1;
-  }
-
-  oldButton = newButton ;
-
-  if(wantToTurn == 1 && wantToTurn != oldWantToTurn){
-      int newPieceRotation = ( currentPieceRotation + 1 ) % 4; 
-      if(pieceCanFit(currentPieceX, currentPieceY, newPieceRotation)){
-          currentPieceRotation = newPieceRotation;
-      }    
-  }
-  
-  oldWantToTurn = wantToTurn;
-  
-}
-
-void tryToDropPiece(){
-  erasePiece();
-
-  if(pieceCanFit(currentPieceX, currentPieceY + 1, currentPieceRotation)){
-    currentPieceY++;
-    placePiece();
-  }
-  else{
-     placePiece();
-     removeRows();
-    if(gameIsOver() == 1){
-        gameOver();
-     }
-
-    chooseNewPiece();
-  }
-}
-
-int gameIsOver(){
-  int x,y,k = 0;
-  bool newPiece[4][4];  
-    for(int i = currentPieceRotation * 4;i < currentPieceRotation * 4 + 4; ++i){
-        for(int j = 0; j < 4; ++j)
-          newPiece[k][j] = currentPiece [i][j];
-        k++;
-      } 
-  for(y = 0; y < 4; ++y){
-    for(x = 0; x < 4; ++x){
-      int newY = currentPieceY + y;
-      int newX = currentPieceX + x;
-      if(newPiece[y][x] > 0) 
-        if(newY < 0) return 1;
     }
   }
   return 0;
 }
 
-void reactToPlayer(){
-    int callMenu =  digitalRead(MENU_BTN);
-    if(callMenu != 0)
-        menu();
-    Serial.print(callMenu);
-    erasePiece();  
-    movePiece();
-    rotatePiece();
-    placePiece();
+int pieceHitsBottom(int pieceX, int pieceY, int pieceRotation) {
+  int k = 0;
+  bool newPiece[4][4];
+  for (int i = pieceRotation * 4; i < pieceRotation * 4 + 4; ++i) {
+    for (int j = 0; j < 4; ++j)
+      newPiece[k][j] = currentPiece[i][j];
+    k++;
+  }
+  for (int y = 0; y < 4; ++y) {
+    int ny = pieceY + y;
+    if (ny < 0) continue;
+    for (int x = 0; x < 4; ++x) {
+      int nx = pieceX + x;
+      if (newPiece[y][x] > 0) {
+        if (ny >= 8) return 1; // yes: goes off bottom of grid      
+        if (matrix[ny][nx] == 1) return 1; // yes: grid already full in this space
+      }
+    }
+  }
+  return 0; // doesn't hit
 }
 
-void gameOver(){
-  int x,y;
-  while(1){
-      if(digitalRead(1) == 0){
-        menu();
-        return;
-      }
+int pieceCanFit(int pieceX, int pieceY, int pieceRotation) {
+  if (pieceOffEdge(pieceX, pieceY, pieceRotation))
+    return 0;
+  if (pieceHitsBottom(pieceX, pieceY, pieceRotation))
+    return 0;
+  return 1;
+}
+
+void erasePiece() {
+  int i, j, k = 0;
+  bool newPiece[4][4] = { 0 };
+  for (i = currentPieceRotation * 4; i < currentPieceRotation * 4 + 4; ++i) {
+    for (j = 0; j < 4; ++j)
+      newPiece[k][j] = currentPiece[i][j];
+    k++;
+  }
+  for (i = 0; i < 4; ++i)
+    for (j = 0; j < 4; ++j) {
+      int nx = currentPieceX + j;
+      int ny = currentPieceY + i;
+      if (ny < 0 || ny > 7) continue;
+      if (nx < 0 || nx > 7) continue;
+      if (newPiece[i][j] == true)
+        matrix[ny][nx] = false;
     }
 }
 
-void drawMatrix(){
-  for(int y = 0; y < 8; ++y)
-    for(int x = 0; x < 8; ++x)
+void placePiece() {
+
+  int i, j, k = 0;
+  bool newPiece[4][4] = {
+    0
+  };
+  for (i = currentPieceRotation * 4; i < currentPieceRotation * 4 + 4; ++i) {
+    for (j = 0; j < 4; ++j)
+      newPiece[k][j] = currentPiece[i][j];
+    k++;
+  }
+  for (i = 0; i < 4; ++i)
+    for (j = 0; j < 4; ++j) {
+      int nx = currentPieceX + j;
+      int ny = currentPieceY + i;
+      if (ny < 0 || ny > 7) continue;
+      if (nx < 0 || nx > 7) continue;
+      if (newPiece[i][j] == true)
+        matrix[ny][nx] = true;
+    }
+}
+
+void movePiece() {
+  int valX = analogRead(JOY_X);
+
+  int newPieceX = 0;
+
+  if (valX < 400)
+    newPieceX = -1;
+  if (valX > 600)
+    newPieceX = 1;
+
+  if (newPieceX != oldPieceX && pieceCanFit(currentPieceX + newPieceX, currentPieceY, currentPieceRotation) == 1) {
+    currentPieceX += newPieceX;
+  }
+  oldPieceX = newPieceX;
+}
+
+void rotatePiece() {
+  int wantToTurn = 0;
+  int newButton = digitalRead(JOY_BTN);
+
+  if (newButton > 0 && oldButton != newButton) {
+    wantToTurn = 1;
+  }
+
+  oldButton = newButton;
+
+  if (wantToTurn == 1 && wantToTurn != oldWantToTurn) {
+    int newPieceRotation = (currentPieceRotation + 1) % 4;
+    if (pieceCanFit(currentPieceX, currentPieceY, newPieceRotation)) {
+      currentPieceRotation = newPieceRotation;
+    }
+  }
+
+  oldWantToTurn = wantToTurn;
+
+}
+
+void tryToDropPiece() {
+  erasePiece();
+
+  if (pieceCanFit(currentPieceX, currentPieceY + 1, currentPieceRotation)) {
+    currentPieceY++;
+    placePiece();
+  } else {
+    placePiece();
+    removeRows();
+    if (gameIsOver() == 1) {
+      gameOver();
+    }
+
+    chooseNewPiece();
+  }
+}
+
+int gameIsOver() {
+  int x, y, k = 0;
+  bool newPiece[4][4];
+  for (int i = currentPieceRotation * 4; i < currentPieceRotation * 4 + 4; ++i) {
+    for (int j = 0; j < 4; ++j)
+      newPiece[k][j] = currentPiece[i][j];
+    k++;
+  }
+  for (y = 0; y < 4; ++y) {
+    for (x = 0; x < 4; ++x) {
+      int newY = currentPieceY + y;
+      int newX = currentPieceX + x;
+      if (newPiece[y][x] > 0)
+        if (newY < 0) return 1;
+    }
+  }
+  return 0;
+}
+
+void reactToPlayer() {
+  int callMenu = digitalRead(MENU_BTN);
+  if (callMenu > 0)
+    menu();
+  erasePiece();
+  movePiece();
+  rotatePiece();
+  placePiece();
+}
+
+void gameOver() {
+  highScoreReplace();
+  menu();
+}
+
+void drawMatrix() {
+  for (int y = 0; y < 8; ++y)
+    for (int x = 0; x < 8; ++x)
       lc.setLed(0, y, x, matrix[y][x]);
 }
 
-
-void setup(){
+void setup() {
   Serial.begin(9600);
   // the zero refers to the MAX7219 number, it is zero for 1 chip
   lc.shutdown(0, false); // turn off power saving, enables display
   lc.setIntensity(0, 15); // sets brightness (0~15 possible values)
-  lc.clearDisplay(0);// clear screen
-  pinMode(JOY_BTN,INPUT);
-  pinMode( A2, INPUT);;
+  lc.clearDisplay(0); // clear screen
+  pinMode(JOY_BTN, INPUT);
+  pinMode(A2, INPUT);;
   chooseNewPiece();
   randomSeed(0);
   lastMove = millis();
   lastDrop = lastMove;
   lcd.begin(16, 2);
   //lcd.clear();
-  lcd.setCursor(2, 7);  // 2 = a cata casuta din linie sa fie aprinsa
-                    
- 
+  lcd.setCursor(2, 7); // 2 = a cata casuta din linie sa fie aprinsa
+
   pinMode(V0_PIN, OUTPUT); // PWN in loc de POTENTIOMETRU
   analogWrite(V0_PIN, 90); // PWN in loc de POTENTIOMETRU
 
 }
 
-void loop()
-{
-  
-  
-  if(millis() - lastMove > moveDelay){
-      lastMove =  millis();
-      reactToPlayer();
-    }
-  
-  if(millis() - lastDrop > dropDelay){
+void loop() {
+
+  if (millis() - lastMove > moveDelay) {
+    lastMove = millis();
+    reactToPlayer();
+  }
+
+  if (millis() - lastDrop > dropDelay) {
     lastDrop = millis();
-    tryToDropPiece();  
+    tryToDropPiece();
   }
 
   drawMatrix();
